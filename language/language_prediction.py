d74cf3e2ad049b95410616d37e52dff0b57a5c2d
Mauro Mackinze <mauro.mackinze@gmail.com>
2020-05-16 13:50:25 -0300

    Se agregan funciones para eliminar urls, hashtags y mentions
    Se modifica la get_language para que elimine urls, hashtags y mentions antes de detectar el lenguaje

--- language/language_prediction.py
+++ language/language_prediction.py
@@ -3,8 +3,31 @@ import numpy as np
 import time
 from datetime import timedelta
 
+import re
 
 import fasttext
+
+
+# Guardamos la regex compiladas porque es más rápido para usarlas muchas veces después.
+# https://www.geeksforgeeks.org/python-check-url-string/
+__regexUrl = regexUrl = re.compile(r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))")
+# https://www.regextester.com/95875
+__regexHashtagsAndMentions = re.compile(r"\s([@#][\w_-]+)")
+__regexMultipleSpaces = re.compile(r"\s+")
+
+def replaceUrls(text):
+    return __regexUrl.sub("", text)
+
+def replaceHashtagsAndMentions(text):
+    # Algunos hashtags están separados de la palabra, y lo mismo con los mentions.
+    # Aprovechamos también por las dudas para separar el hashtag/mention de la palabra anterior.
+    text = text.replace("# ", "#").replace("#", " #" ).replace("@ ", "@").replace("@", " @" )
+    text = __regexMultipleSpaces.sub(" ", text)
+    return __regexHashtagsAndMentions.sub("", text)
+
+def replaceUrlsHashtagsAndMentions(text):
+    return replaceHashtagsAndMentions(replaceUrls(text))
+
 
 #Source: https://github.com/indix/whatthelang
 MODEL_FILE = "lid.176.ftz"
@@ -15,6 +38,8 @@ def get_language(text):
     result = ["-",0.0]
 
     if (type(text) == str):
+        # Para obtener el idioma sin eliminar las urls, los hashtags y los mentions, comentar la siguiente línea.
+        text = replaceUrlsHashtagsAndMentions(text)
         if (len(text) > 10):
             prediction = model.predict(text)
             if len(prediction) > 0:
