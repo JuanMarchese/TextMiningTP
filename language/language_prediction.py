ea0ee92cf671992a6d5396f19bee588916bafab3
Mauro Mackinze <mauro.mackinze@gmail.com>
2020-05-17 14:44:25 -0300

    Se consideran casos especiales para los que fallaban las regex

--- language/language_prediction.py
+++ language/language_prediction.py
@@ -9,19 +9,32 @@ import fasttext
 
 
 # Guardamos la regex compiladas porque es más rápido para usarlas muchas veces después.
+__regexSpace = re.compile(r"\s")
 # https://www.geeksforgeeks.org/python-check-url-string/
-__regexUrl = regexUrl = re.compile(r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))")
+__regexUrl = re.compile(r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))")
+__regexSimpleUrl = re.compile(r"\shttps?://\S+")
 # https://www.regextester.com/95875
 __regexHashtagsAndMentions = re.compile(r"\s([@#][\w_-]+)")
 __regexMultipleSpaces = re.compile(r"\s+")
+__regexMultiplePunctuations = re.compile(r"\.+")
 
 def replaceUrls(text):
+    
+    # Algunos textos no tienen ningún espacio (ni \r\t\n\f). Los consideramos basura, porque no vamos a poder
+    # clasificar el idoma de todas formas.
+    if(not __regexSpace.search(text)):
+        return ""
+
+    # Reemplazamos los puntos duplicados por un solo punto (si hay muchos puntos se rompe la regex de búsqueda de urls).
+    text = __regexMultiplePunctuations.sub(".", text)
+    text = __regexSimpleUrl.sub(" ", text) # Acá reemplazamos por un espacio porque los captura la regex
     return __regexUrl.sub("", text)
 
 def replaceHashtagsAndMentions(text):
     # Algunos hashtags están separados de la palabra, y lo mismo con los mentions.
     # Aprovechamos también por las dudas para separar el hashtag/mention de la palabra anterior.
     text = text.replace("# ", "#").replace("#", " #" ).replace("@ ", "@").replace("@", " @" )
+    # Reemplazamos los espacios duplicados por un solo espacio.
     text = __regexMultipleSpaces.sub(" ", text)
     return __regexHashtagsAndMentions.sub("", text)
 
